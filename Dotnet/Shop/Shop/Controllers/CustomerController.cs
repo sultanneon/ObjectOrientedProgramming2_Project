ce68ef77f25a013aade19329a2b02be2719e9a29
sultanneon <neonsultan1999@gmail.com>
2021-11-01 23:45:53 +0600

    SHOP

---
+++ Dotnet/Shop/Shop/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class CustomerController : Controller
+    {
+       
+      
+        public ActionResult Profiles(int id)
+        {
+            var db = new ShoppEntities();
+            var customer = (from s in db.Users
+                            where s.UserID == id
+                            select s).FirstOrDefault();
+            return View(customer);
+        }
+        public ActionResult Edit(int Id)
+        {
+            var db = new ShoppEntities();
+            var student = (from s in db.Users
+                           where s.UserID == Id
+                           select s).FirstOrDefault();
+
+            return View(student);
+        }
+        [HttpPost]
+        public ActionResult Edited(User st)
+        {
+            var db = new ShoppEntities();
+            var student = (from s in db.Users
+                           where s.UserID == st.UserID
+                           select s).FirstOrDefault();
+            db.Entry(student).CurrentValues.SetValues(st);
+            db.SaveChanges();
+            return RedirectToAction("Index","Customer",st);
+        }
+
+    }
+}
---
+++ Dotnet/Shop/Shop/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+     
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string Username, string Password)
+        {
+            var db = new ShoppEntities();
+            var t = (from s in db.Users
+                            where s.Username == Username && s.Password == Password
+                            select s).FirstOrDefault();
+      
+            if (t != null)
+            {
+                FormsAuthentication.SetAuthCookie(t.Username, true);
+                return RedirectToAction("Index", "Customer",t);
+            }
+            return View();
+        }
+
+    }
+}
